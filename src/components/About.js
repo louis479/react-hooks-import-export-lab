b1252b9e0c23572f18ee679b5f593d470192ca04
louis-ngugi <louisngugi858@gmail.com>
2025-01-19 01:52:57 +0300

    test react in About.js NavBar.js user.js Appjs and Homejs

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,10 @@ import React from "react";
 function About() {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <h2>About</h2>
     </div>
   );
 }
+
+
+export default About;
